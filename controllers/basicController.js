7d1d1f55abb8eab0c8ec380a4750efb6e0750463
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-11-30 13:43:32 +0100

    update: logic to getById, editTodo, and replaceTodo

--- controllers/basicController.js
+++ controllers/basicController.js
@@ -12,14 +12,88 @@ async function getTodo(request, response) {
 
 async function createTodo(request, response) {
   const todo = request.body;
-  console.log(request.body)
+  const { title, description } = todo;
+
+  if (!title || !description) {
+    return response.status(409).json({
+      error: "Validation Error",
+      message: "Invalid Input",
+    });
+  }
+
   db.push(todo);
   return response.status(201).json({
     message: "Todo Successfully created",
   });
 }
 
+async function getById(request, response) {
+  const { id } = request.params;
+  const todo = db.find((db) => db.id === parseInt(id));
+
+  if (!todo) {
+    return response.status(404).json({
+      error: "Resource Not Found",
+      message: "Could not find that Todo",
+    });
+  }
+
+  return response.json(todo);
+}
+
+async function replaceTodo(request, response) {
+  const { id } = request.params;
+  const newTodo = request.body;
+  const { title, description } = newTodo;
+
+  if (!title || !description) {
+    return response.status(409).json({
+      error: "Validation Error",
+      message: "Invalid Input",
+    });
+  }
+
+  const todo = db.find((db) => db.id === parseInt(id));
+
+  if (!todo) {
+    return response.status(404).json({
+      error: "Resource Not Found",
+      message: "Could not find that Todo",
+    });
+  }
+
+  todo.title = newTodo.title;
+  todo.description = newTodo.description;
+
+  return response.status(200).json({ message: "Successfully replaced todo" });
+}
+
+async function editTodo(request, response) {
+  const { id } = request.params;
+  const newTodo = request.body;
+  const { title, description } = newTodo;
+
+  const todo = db.find((db) => db.id === parseInt(id));
+
+  if (!todo) {
+    return response.status(404).json({
+      error: "Resource Not Found",
+      message: "Could not find that Todo",
+    });
+  }
+
+  todo.title = title ? title : todo.title;
+  todo.description = description ? description : todo.description;
+
+  return response.status(200).json({
+    message: "Successfully edited todo",
+  });
+}
+
 module.exports = {
   getTodo,
+  getById,
   createTodo,
+  replaceTodo,
+  editTodo,
 };
